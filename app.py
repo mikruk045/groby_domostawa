1d04c46efaa9d0712b4933f458d90455a39c3e2a
mikruk045 <59932274+mikruk045@users.noreply.github.com>
2021-07-22 17:26:07 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def database_edit(id):
                 conn.execute(""" update zmarli set nazwisko = '{}', imie = '{}', data_urodzenia = '{}', data_zgonu = '{}', przyczyna = '{}', id_miejscowosc = '{}', nr_adres = '{}', id_admin = '{}', inf_dodat = '{}' where id = '{}' """.format(nazwisko, imie, data_ur, data_zg, przyczyna, id_miejscowosci[0]['id_miejscowosci'], nr_adres, session.get('id_admin')['id_admin'], info_dodat, id_obiektu))
             conn.execute(""" update zmarli_kwatery set id_kwatera = '{}' where id_zmarly = '{}' """.format(kwatera, id_obiektu))
             data2 = [json.dumps(session['id_admin']), json.dumps(session['imie_nazwisko'])]
-            return redirect(url_for('/database'))
+            return redirect(url_for('database'))
     else:
         return render_template('database.html')
 
